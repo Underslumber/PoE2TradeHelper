11c2ccc57f2b45cad84737341080fa3f54936911
XapcT <hatzy@mail.ru>
2026-05-12 13:25:18 +0300

    Add app icon to header

--- tests/test_icons.py
+++ tests/test_icons.py
@@ -13,5 +13,7 @@ def test_app_icon_has_transparent_background():
     svg = (Path(__file__).resolve().parents[1] / "app" / "web" / "static" / "icon.svg").read_text(encoding="utf-8")
     canvas_path = "M0 0 C378.84 0 757.68 0 1148 0 C1148 306.24 1148 612.48 1148 928"
 
+    assert '<clipPath id="icon-shape">' in svg
+    assert "fill: #000000" not in svg
     assert f'{canvas_path} C769.16 928 390.32 928 0 928 C0 621.76 0 315.52 0 0 Z "' not in svg
     assert f'{canvas_path} C769.16 928 390.32 928 0 928 C0 621.76 0 315.52 0 0 Z M570.25' not in svg
